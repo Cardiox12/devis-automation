174b9a655628bf3c41c2ac1b0841feb5ae864459
Tony Bellavia <at.bellavia@gmail.com>
2020-07-20 11:56:49 +0200

    Finish updating price

--- static/scripts/add_to_list.js
+++ static/scripts/add_to_list.js
@@ -15,4 +15,17 @@ function add_to_list(item){
 	`;
 	const nodes = new DOMParser().parseFromString(html, "text/html").body.childNodes[0];
 	quote_list.appendChild(nodes);
+	get_price();
+}
+
+function get_price(){
+	const price_label = document.querySelector(".quote-resume-price-dyn-item").querySelector("span");
+	const items = document.querySelector("ul").children;
+	let acc = 0;
+
+	for (const item of items){
+		const price = item.querySelector(".quote-list-item-price").querySelector("span");
+		acc += parseInt(price.innerHTML, 10);
+	}
+	price_label.innerHTML = `${acc}`;
 }
