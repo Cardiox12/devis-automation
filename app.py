1ffd7b790dcb499605800ecec8459a4bddec0277
Tony Bellavia <at.bellavia@gmail.com>
2020-07-20 04:18:35 +0200

    Finish refactoring template source code with jinja

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
 @app.route("/")
 def hello():
 	return render_template('pages/home.html')
