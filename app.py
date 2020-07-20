4cb686deaddee663856d12c67b6b484257b594b9
Tony Bellavia <at.bellavia@gmail.com>
2020-07-20 03:36:44 +0200

    Finish handling 404 error

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello():
 	return render_template('pages/home.html')
 
+@app.errorhandler(404)
+def page_not_found(error):
+	return render_template("pages/404.html"), 404
+
 if __name__ == "__main__":
 	app.run(debug=True, port=80)
