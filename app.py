16c528f05ed79c9080a3b69e04022683408f3d57
Tony Bellavia <at.bellavia@gmail.com>
2020-07-22 18:54:27 +0200

    Finish json config of services

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from pprint import pprint
 import json
 
 app = Flask(__name__)
@@ -7,26 +8,35 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-	config = None
+	services = None
 
 	if request.method == "POST":
 		data = request.form.items()
-		config = []
+		services = []
 
-		for items in zip(*[iter(data)] * 2):
+		for items in zip(*[iter(data)] * 3):
 			infos = []
 			for item in items:
 				infos.append(item[1])
-			config.append(infos)
+			services.append(infos)
 
 	with open("config/prestations.json", "r") as f:
 		data = json.load(f)
 
-		if config != None:
-			if len(config) == 0:
-				print("No config")
-			else:
-				print(config)
+		if services != None:
+			if len(services) != 0:
+				export = []
+
+				for service in services:
+					export.append({
+						"name" : service[1],
+						"price" : service[0],
+						"description" : service[2]
+					})
+
+				with open("config/export.json", "w") as outfile:
+					json.dump({"services" : export}, outfile, indent=4, ensure_ascii=False)
+
 		return render_template("pages/home.html", quotes=data)
 
 @app.errorhandler(404)
--- static/scripts/main.js
+++ static/scripts/main.js
@@ -5,6 +5,9 @@ function add_to_list(item){
 	const quote_title = card.querySelector(".quote-card-title").querySelector("h3").innerHTML;
 	const quote_price = card.querySelector(".quote-card-footer-price").querySelector("input").value;
 	const quote_list = document.querySelector(".quote-list").querySelector("ul");
+	const quote_desc = item.parentNode.parentNode.parentNode.querySelector(".quote-card-desc").querySelector("p").innerHTML;
+
+	console.log(quote_desc)
 
 	const html = `
 		
@@ -16,6 +19,7 @@ function add_to_list(item){
 			</div>
 			<input type="hidden" name="price,${counter}" value="${quote_price}">
 			<input type="hidden" name="title,${counter}" value="${quote_title}">
+			<input type="hidden" name="desc,${counter}" value="${quote_desc}">
 		</li>
 	`;
 	counter++;
